d613be0d94c42656d5e65fc5304284e88670bf31
farisqlail11 <farisqlail@gmail.com>
2022-03-31 00:16:37 +0700

    array 0 profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -15,25 +15,27 @@
             <div class="col-10">
                 <div class="card">
                     <div class="card-body">
-                        <p class="card-text">Nama : {{ $sertifikat[0]->nama }}</p>
-                        <p class="card-text">NIP : {{ $sertifikat[0]->nip }}</p>
-                        <p class="card-text">No Telepon : {{ $sertifikat[0]->telp }}</p>
-                        <p class="card-text">Pelatihan : 
-                            @if ($sertifikat[0]->nama_pelatihan == null)
-                            Belum ada pelatihan
-                            @else
-                            {{ $sertifikat[0]->nama_pelatihan }}
-                            @endif
-                        </p>
-                        <p class="card-text">Sertifikat : 
-                            @if ($sertifikat[0]->foto_sertifikat == null)
-                                Belum ada sertifikat
-                            @else
+                        @if ($sertifikat->count() > 0)
+                            <p class="card-text">Nama : {{ $sertifikat[0]->nama }}</p>
+                            <p class="card-text">NIP : {{ $sertifikat[0]->nip }}</p>
+                            <p class="card-text">No Telepon : {{ $sertifikat[0]->telp }}</p>
+                            <p class="card-text">Pelatihan : 
+                                @if ($sertifikat[0]->nama_pelatihan == null)
+                                Belum ada pelatihan
+                                @else
+                                {{ $sertifikat[0]->nama_pelatihan }}
+                                @endif
+                            </p>
+                            <p class="card-text">Sertifikat : 
+                                @if ($sertifikat[0]->foto_sertifikat == null)
+                                    Belum ada sertifikat
+                                @else
                                 <img src="{{ asset('storage/'.$sertifikat[0]->foto_sertifikat) }}" alt="" height="128">
-                                <br><br>
-                                <a href="{{ asset('storage/'.$sertifikat[0]->foto_sertifikat) }}" class="btn btn-success" download="">Download Sertifikat</a>
-                            @endif
-                        </p>
+                                    <br><br>
+                                    <a href="{{ asset('storage/'.$sertifikat[0]->foto_sertifikat) }}" class="btn btn-success" download="">Download Sertifikat</a>
+                                    @endif
+                                </p>
+                        @endif
 
                     </div>
                 </div>
