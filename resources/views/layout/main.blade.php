f3f456f32764b6094ecf58d03593c748b28166ab
farisqlail11 <farisqlail@gmail.com>
2022-04-09 01:10:00 +0700

    required input

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -26,6 +26,7 @@
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
       <script src="https://cdn.ckeditor.com/4.14.0/standard/ckeditor.js"></script>
       @stack('script')
-
+      @include('sweetalert::alert')
+      
   </body>
 </html>
